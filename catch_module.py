c754115a5a9073b7a9d6a4d0cafbec09027c5922
Remco <remco-leijenaar@live.com>
2023-05-05 13:03:02 +0200

    Added training_step to catch module

--- catch_module.py
+++ catch_module.py
@@ -1,13 +1,15 @@
 from typing import Any
-from pytorch_lightning import LightningModule
 
+import torch
+from pytorch_lightning import LightningModule
+from torch import Tensor, nn
+from torch.optim import Adam, Optimizer, RMSprop
 from torch.utils.data import DataLoader
-from torch.optim import Optimizer, RMSprop, Adam
+
+from agent import QNetworkAgent
 from catch import CatchEnv
 from dnn import DeepQNetwork
-
-from memory import ReplayBuffer, ReplayBufferDataset
-from agent import QNetworkAgent
+from memory import ReplayBuffer, ReplayBufferDataset, Trajectory
 from scheduler import EpsilonDecay
 
 
@@ -21,6 +23,7 @@ class CatchRLModule(LightningModule):
                  epsilon_decay_steps: float = 1000,
                  buffer_capacity: int = 1000,
                  replay_warmup_steps: int = 10,
+                 target_net_update_freq: int = 100,
                  *args: Any,
                  **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
@@ -44,6 +47,12 @@ class CatchRLModule(LightningModule):
         self.agent = QNetworkAgent(
             self.env, self.Q_network, self.replay_buffer, epsilon_schedule)
 
+        self.loss = nn.MSELoss()
+        self.episode_reward = 0
+
+    def forward(self, x):
+        return self.Q_network(x)
+
     def replay_warmup(self):
         for _ in range(self.hparams.replay_warmup_steps):
             self.agent.step()
@@ -53,11 +62,47 @@ class CatchRLModule(LightningModule):
         # Copy the weights from Q_network to target_Q_network
         self.target_Q_network.load_state_dict(self.Q_network.state_dict())
 
+    @torch.no_grad()
+    def compute_td_target(self, batch: Trajectory) -> Tensor:
+        Q_values = self.target_Q_network(batch.next_state).max(dim=-1)
+        Q_values[batch.terminal] = 0.  # Terminal state has 0 Q-value
+        td_target = batch.reward + self.hparams.gamma * Q_values
+        return td_target
+
+    def training_step(self, batch: Trajectory, batch_idx: int) -> Tensor:
+        reward, terminal = self.agent.step()
+
+        td_target = self.compute_td_target(batch)
+        Q_values = self.Q_network(batch.state).gather(
+            dim=-1, index=batch.action)
+
+        loss = self.loss(Q_values, td_target)
+
+        if self.hparams.global_step % self.hparams.target_net_update_freq == 0:
+            self.update_target_network()
+
+        # Logging
+        self.episode_reward += reward
+        if terminal:
+            self.episode_reward = 0
+            self.log("episode reward", self.episode_reward,
+                     on_step=True, on_epoch=False, prog_bar=True)
+
+        self.log('epsilon', self.agent.epsilon, on_step=True, on_epoch=False)
+        self.log_dict({'train_loss': loss,
+                       'step': self.hparams.global_step,
+                       'reward': reward,
+                       }, on_step=True, on_epoch=False, prog_bar=True)
+
+        return loss
+
     def train_dataloader(self) -> DataLoader:
+        # First call
         if len(self.replay_buffer) < self.hparams.replay_warmup_steps:
             self.replay_warmup()
         dataset = ReplayBufferDataset(self.replay_buffer)
         return DataLoader(dataset, batch_size=self.hparams.batch_size)
 
     def configure_optimizers(self) -> Optimizer:
-        return Adam(self.parameters(), lr=self.hparams.learning_rate)
+        # Only the Q-network's parameters are optimized
+        return Adam(self.Q_network.parameters(), lr=self.hparams.learning_rate)
