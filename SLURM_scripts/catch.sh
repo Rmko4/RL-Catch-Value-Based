62d39494a9a1fd84e57eeb97e650703c28f1b49e
Remco <remco-leijenaar@live.com>
2023-05-11 16:49:07 +0200

    Fixed default parameters

--- SLURM_scripts/catch.sh
+++ SLURM_scripts/catch.sh
@@ -18,16 +18,16 @@ cd $TMPDIR/Deep-Reinforcement-Learning/
 
 python train_agent.py \
 --run_name train \
---max_epochs 100 \
---batch_size 64 \
+--max_epochs 50 \
+--batch_size 128 \
 --learning_rate 0.001 \
 --gamma 0.99 \
 --epsilon_start 0.5 \
 --epsilon_end 0.01 \
---epsilon_decay_rate 500 \
+--epsilon_decay_rate 100 \
 --buffer_capacity 10000 \
 --replay_warmup_steps 10 \
 --soft_update_tau 0.05 \
---hidden_size 128 \
+--hidden_size 256 \
 --n_filters 16 \
 --algorithm DQV
